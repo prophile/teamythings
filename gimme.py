f8929253910210cda84571048973f2407ee94f3c
Alastair Lynn <arplynn@gmail.com>
2013-04-14 10:07:08 +0100

    Fix all the things and test

--- gimme.py
+++ gimme.py
@@ -11,15 +11,15 @@ for team in TEAMS:
 def gimme(team):
     tm = team_db[team.upper()]
     if 'team' in tm:
-        print "{0}: {1}".format(tm['id'], tm['team'])
+        print "{0}: {1}".format(tm['id'], tm['team'].encode('utf-8'))
     else:
         print tm['id']
     if 'college' in tm:
-        print "{0}".format(tm['college'])
+        print "{0}".format(tm['college'].encode('utf-8'))
     for note in tm.get('notes', ()):
         items = note.split('-')
         for item in items:
-            print " - {0}".format(item.strip())
+            print " - {0}".format(item.strip().encode('utf-8'))
     print ""
 
 import sys
